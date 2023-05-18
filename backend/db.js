78e1797b8fcf8b2f6d4378c6f0df45ae588a734e
Mohd Samiullah <samiullah0813@gmail.com>
2023-05-18 05:39:02 -0700

    first commit

---
+++ backend/db.js
@@ -0,0 +1,78 @@
+const mongoose = require('mongoose');
+const mongoURI = "mongodb+srv://samiullah0813:Sami123@inotebook.sruxo0x.mongodb.net/?retryWrites=true&w=majority"
+const connectToMongo = async () => {
+    try {
+        // mongoose.set('strictQuery', false)
+        mongoose.connect(mongoURI,
+            { useNewUrlParser: true, useUnifiedTopology: true }) 
+        console.log('Mongo atlas connected')
+    } catch(error) {
+        console.log(error)
+        process.exit()
+    }
+}
+module.exports = connectToMongo;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const connectDB = async () => {
+//     try {
+//         mongoose.set('strictQuery', false)
+//         mongoose.connect(process.env.MONGO_URI) 
+//         console.log('Mongo connected')
+//     } catch(error) {
+//         console.log(error)
+//         process.exit()
+//     }
+// }
+
+// mongoose.connect(mongoURI,  (err) =>{
+    //     if (err) console.log(`unable to connect to the server :${err}`);
+    //     else
+    //         console.log('MongoDB is connected');
+    // })
+
+    // mongoose
+    // .connect(mongoURI)
+    // .catch (error => console.log(error));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 'mongodb://127.0.0.1:27017/test'
