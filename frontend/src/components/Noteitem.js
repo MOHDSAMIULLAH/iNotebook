8c40fce234a24ee68b07bdf919f31c4ec0af5158
Mohd Samiullah <samiullah0813@gmail.com>
2023-09-25 08:02:12 -0700

    icons shift

--- frontend/src/components/Noteitem.js
+++ frontend/src/components/Noteitem.js
@@ -10,9 +10,10 @@ const Noteitem = (props) => {
         <div className="col-md-3">
             <div className="card my-3">
                 <div className="card-body">
-                    <div className="d-flex align-items-center">
-                        <h5 className="card-title">{note.title}</h5><i className="far fa-trash-alt mx-2" onClick={()=>{deleteNote(note._id);props.showAlert("Deleted Successfully","success");}}></i>
-                        <i className="far fa-edit mx-2" onClick={()=>{updateNote(note)}}></i>
+                    <div className="justify-content-between flex align-items-center position-relative">
+                        <div className='flex justify-content-end float-right text-right' style={{width: "4rem"}}><i className="far fa-trash-alt mx-2" onClick={()=>{deleteNote(note._id);props.showAlert("Deleted Successfully","success");}}></i>
+                        <i className="far fa-edit mx-2 align-self-end" onClick={()=>{updateNote(note)}}></i></div>
+                        <h5 className="card-title flex-wrap w-80">{note.title}</h5>
                     </div>
                     <p className="card-text ">{note.description}</p>
                     <p className="card-tag text-secondary">{note.tag}</p>
